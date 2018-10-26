aeb6c0b1362d832c095649b27e22de841ffd7f38
MegaMeganum <u201516808@upc.edu.pe>
2018-10-26 11:59:29 -0500

    Second project

---
+++ iMovie_Night/iMovie_Night/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMovie_Night
+//
+//  Created by Karen Galindo on 10/26/18.
+//  Copyright © 2018 UPC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
