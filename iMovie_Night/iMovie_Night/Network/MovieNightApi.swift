e998b4b9ff9a1a7cbaa84560fa61fdfa36417fff
kevinorh <kevinorh@hotmail.com>
2018-11-07 02:13:06 -0500

    Post login working  with static data / To do: capture token from Header

--- iMovie_Night/iMovie_Night/Network/MovieNightApi.swift
+++ iMovie_Night/iMovie_Night/Network/MovieNightApi.swift
@@ -11,7 +11,7 @@ import Alamofire
 
 class MovieNightApi {
     
-    static let baseUrl = "https://nodejsmovienight20182.herokuapp.com/api/v2/"
+    static let baseUrl = "http://nodejsmovienight20182.herokuapp.com/api/v2/"
     
     static func handleError(error: Error) {
         print("Error while requesting Data: \(error.localizedDescription)")
@@ -36,8 +36,31 @@ class MovieNightApi {
             }
     }
     
-    public static var postLoginUrl:String{
-        return "\(baseUrl)signin" }
+    static func postLoginUrl (responseHandler: @escaping(SigninResponse) -> (Void),
+    errorHandler: (@escaping(Error) -> (Void))=handleError){
+        let parameters: [String: Any] = ["email": "CesarCas@hotmail.com", "password": "miupc.456."]
+       Alamofire.request("\(baseUrl)signin",
+                          method: .post,
+                          parameters: parameters,
+                          encoding: JSONEncoding.default)
+            .validate()
+            .responseJSON(completionHandler: {(response) in
+                switch response.result{
+                case .success(let value):
+                    do{
+                        let data = try JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+                        let decoder = JSONDecoder()
+                        let signinResponse = try decoder.decode(SigninResponse.self, from: data)
+                        responseHandler(signinResponse)
+                    }catch{
+                        print("\(error)")
+                    }
+                case .failure(let error):
+                    errorHandler(error)
+                }
+            })
+        
+    }
     
     public static var postRegisterUrl:String{
         return "\(baseUrl)signup" }
